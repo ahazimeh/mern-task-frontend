1d6452f45858c321bcf0fe560bd47da5b4ae3cd7
ahazimeh <hazimeh95@gmail.com>
2022-12-02 23:44:21 +0200

    require auth

--- src/RequireAuth.tsx
+++ src/RequireAuth.tsx
@@ -1,8 +1,10 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const RequireAuth = () => {
+  const token = localStorage.getItem("token");
   const location = useLocation();
-  return <Navigate to="/" state={{ from: location }} replace />;
-  //   return <Outlet />;
+  if (!token)
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  return <Outlet />;
 };
 export default RequireAuth;
